e345be411b2b538b7864b622ad1c8f26256b5141
Adem ilter <ademilter@gmail.com>
2019-10-02 14:52:31 +0300

    gereksiz dosyalar silindi

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const BlogPostPreview = ({ entry, widgetFor }) => (
-  <div
-    content={widgetFor("body")}
-    description={entry.getIn(["data", "description"])}
-    tags={entry.getIn(["data", "tags"])}
-    title={entry.getIn(["data", "title"])}
-  />
+  <div>
+    <div>{widgetFor("body")}</div>
+    <div>{entry.getIn(["data", "description"])}</div>
+    <div>{entry.getIn(["data", "tags"])}</div>
+    <div>{entry.getIn(["data", "title"])}</div>
+  </div>
 );
 
 export default BlogPostPreview;
