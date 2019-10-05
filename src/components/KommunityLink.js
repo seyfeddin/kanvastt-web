182f1bd32b203dfd55d88577afb937bbd2b06b52
Seyfeddin Başsaraç <seyfeddin@wearethread.co>
2019-10-05 12:44:23 +0300

    Add meta assets

---
+++ src/components/KommunityLink.js
@@ -0,0 +1,13 @@
+import React from "react";
+import kommunity from "../img/kommunity.svg";
+
+export default () => (
+  <a
+    class="kommunity"
+    href="https://kommunity.com/kanvas-tasarim-toplulugu"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img src={kommunity} alt="Kommunity" />
+  </a>
+);
