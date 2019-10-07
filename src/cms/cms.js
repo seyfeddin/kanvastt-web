4dfc0597e122da5da1241c2f045d5a736f56da24
Seyfeddin Başsaraç <seyfeddin@wearethread.co>
2019-10-07 13:17:19 +0300

    Cleans up warnings

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,9 +2,5 @@ import CMS from "netlify-cms-app";
 import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
 
-import EventPreview from "./preview-templates/EventPreview";
-
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
-
-CMS.registerPreviewTemplate("event", EventPreview);
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import addToMailchimp from "gatsby-plugin-mailchimp";
 
 export default class RegisterForm extends React.Component {
