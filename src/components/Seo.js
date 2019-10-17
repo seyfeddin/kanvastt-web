0bea212c66c388bc856b391b9a5199782e7a882b
Seyfeddin Başsaraç <seyfeddin@wearethread.co>
2019-10-17 13:07:39 +0300

    Fix twitter card images

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -33,11 +33,12 @@ const Seo = () => {
       <meta name="twitter:site" content={social.twitter} />
       <meta name="twitter:creator" content={social.twitter} />
       <meta name="twitter:title" content={title} />
+      <meta name="twitter:image" content={`./img/og-image.jpg`} />
 
       <meta property="og:type" content="business.business" />
       <meta property="og:title" content={title} />
       <meta property="og:url" content="/" />
-      <meta property="og:image" content={`/img/og-image.jpg`} />
+      <meta property="og:image" content={`./img/og-image.jpg`} />
 
       <link
         rel="preload"
