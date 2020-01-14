6a9fad11ecb93495f07f33e92bdd1ff673a8d904
Adem ilter <ademilter@gmail.com>
2020-01-14 12:56:12 +0300

    event için fotoğraf albümü eklendi

--- components/event.js
+++ components/event.js
@@ -1,53 +1,76 @@
 import React from 'react'
-import { format } from 'date-fns'
+import { format, isPast } from 'date-fns'
 import { tr } from 'date-fns/locale'
 
-function Event({
-  datetime,
-  title,
-  location_name,
-  location_url,
-  description,
-  register_url
-}) {
+function MetaPast({ photos_url }) {
+  return (
+    photos_url && (
+      <div className="event-meta mt-16">
+        🖼{' '}
+        <a href={photos_url} target="_blank" rel="noopener noreferrer">
+          Etkinlik fotoğrafları
+        </a>
+      </div>
+    )
+  )
+}
+
+function MetaFeature({ datetime, location_name, location_url, register_url }) {
+  return (
+    <ul className="event-meta mt-16 flat-list">
+      {/* datetime */}
+      <li>
+        🕣{' '}
+        {format(new Date(datetime), 'dd MMMM yyyy HH:mm', {
+          locale: tr
+        })}
+      </li>
+
+      {/* location */}
+      {location_name && (
+        <li className="mt-8">
+          📍{' '}
+          {location_url ? (
+            <a href={location_url} target="_blank" rel="noopener noreferrer">
+              {location_name}
+            </a>
+          ) : (
+            location_name
+          )}
+        </li>
+      )}
+
+      {/* register */}
+      {register_url && (
+        <li className="mt-8">
+          🎫{' '}
+          {register_url && (
+            <a href={register_url} target="_blank" rel="noopener noreferrer">
+              Kayıt ol
+            </a>
+          )}
+        </li>
+      )}
+    </ul>
+  )
+}
+
+function Event(props) {
   return (
     <article className="event mt-32 pt-32">
       {/* title */}
-      <h3 className="event-title mt-0">{title}</h3>
+      <h3 className="event-title mt-0">{props.title}</h3>
 
       {/* description */}
-      <p className="event-description mb-0">{description}</p>
+      <p className="event-description mb-0">{props.description}</p>
 
       {/* meta */}
-      {datetime ? (
-        <ul className="event-meta mt-16 flat-list">
-          {/* datetime */}
-          <li>
-            🕣{' '}
-            {format(new Date(datetime), 'dd MMMM yyyy HH:mm', {
-              locale: tr
-            })}
-          </li>
-
-          {/* location */}
-          {location_name && (
-            <li className="mt-8">
-              📍{' '}
-              {location_url ? (
-                <a href={location_url}>{location_name}</a>
-              ) : (
-                location_name
-              )}
-            </li>
-          )}
-
-          {/* register */}
-          {register_url && (
-            <li className="mt-8">
-              🎫 {register_url && <a href={register_url}>Kayıt ol</a>}
-            </li>
-          )}
-        </ul>
+      {props.datetime ? (
+        isPast(new Date(props.datetime)) ? (
+          <MetaPast {...props} />
+        ) : (
+          <MetaFeature {...props} />
+        )
       ) : (
         <p className="mt-16">🕣 Yakında</p>
       )}
